c0b5694821a5ee34297a5f459b1907f64ca334d7
jaznar <32256740+jaznar@users.noreply.github.com>
2018-02-09 08:07:47 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -117,6 +117,20 @@ if (!db) {
 });
 
 app.get('/geterrores', function (req, res) {
+	
+	// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+	
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
@@ -150,9 +164,22 @@ app.get('/geterrorestrace', function (req, res) {
 });
 
 app.get('/geterrorescount', function (req, res) {
+  
+	// Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
   // try to initialize the db on every request if it's not already
   // initialized.
-  
+	
   if (!db) {
     initDb(function(err){});
   }
