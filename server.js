f8a6ab4b165920033589466dff7d5f19243f581c
jaznar <32256740+jaznar@users.noreply.github.com>
2018-02-09 12:00:31 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ app.get('/getgroupbymarca', function (req, res){
 	  intDb(function(err){});
   }
   if (db){
-	db.collection("erroresaccountdb").aggregate({$group : {brand: "$BUILD.BRAND", total: {$sum: 1}}}, 	
+	db.collection("erroresaccountdb").aggregate({$group : {_id: "$BUILD.BRAND", total: {$sum: 1}}}, 	
       function(err,data) {
          if (err) console.log(err);
          res.send( data );
